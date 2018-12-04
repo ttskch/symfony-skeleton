883f8d87419aef78f14d338f1bc01651a69a8b86
Takashi Kanemoto <ttskch@gmail.com>
2018-12-04 16:55:09 +0900

    refactor: fix some deprecations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,16 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Route("/", name="home_")
  */
-class HomeController extends Controller
+class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="index")
