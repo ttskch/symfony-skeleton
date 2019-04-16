70759fb290b2550bba9810fb2604a60203014b16
Takashi Kanemoto <ttskch@gmail.com>
2019-04-16 23:10:09 +0900

    refactor: cosme

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,16 @@ import bsCustomFileInput from 'bs-custom-file-input';
 // somehow this doesn't work...
 // @see https://select2.org/configuration/data-attributes
 // $('select').select2({
-//     theme: 'bootstrap4',
-//     width: '100%',
+//   theme: 'bootstrap4',
+//   width: '100%',
 // });
 $('select').each(function () {
-    $(this).select2({
-        theme: "bootstrap4",
-        width: '100%',
-        placeholder: $(this).data('placeholder'),
-        allowClear: Boolean($(this).data('allow-clear')) || false,
-    });
+  $(this).select2({
+    theme: "bootstrap4",
+    width: '100%',
+    placeholder: $(this).data('placeholder'),
+    allowClear: Boolean($(this).data('allow-clear')) || false,
+  });
 });
 
 $('select[autofocus]').select2('focus');
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const Encore = require('@symfony/webpack-encore');
 
 Encore
-    .setOutputPath('public/build/')
-    .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
-    .autoProvidejQuery()
-    .autoProvideVariables({
-        Popper: ['popper.js', 'default'],
-    })
-    .enableSassLoader()
-    .enablePostCssLoader()
-    .enableVersioning(Encore.isProduction())
-    .enableSourceMaps(!Encore.isProduction())
-    .addEntry('app', [
-        './assets/js/app.js',
-        './assets/scss/app.scss',
-    ])
-    .addEntry('vendors', [
-        // js
-        'jquery',
-        'bootstrap',
-        'popper.js',
-        // styles
-        './assets/scss/vendors.scss',
-    ])
-    .splitEntryChunks()
-    .enableSingleRuntimeChunk()
-    .copyFiles({
-        from: './assets/images',
-        to: 'images/[path][name].[ext]',
-    })
+  .setOutputPath('public/build/')
+  .setPublicPath('/build')
+  .cleanupOutputBeforeBuild()
+  .autoProvidejQuery()
+  .autoProvideVariables({
+    Popper: ['popper.js', 'default'],
+  })
+  .enableSassLoader()
+  .enablePostCssLoader()
+  .enableVersioning(Encore.isProduction())
+  .enableSourceMaps(!Encore.isProduction())
+  .addEntry('app', [
+    './assets/js/app.js',
+    './assets/scss/app.scss',
+  ])
+  .addEntry('vendors', [
+    // js
+    'jquery',
+    'bootstrap',
+    'popper.js',
+    // styles
+    './assets/scss/vendors.scss',
+  ])
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+  .copyFiles({
+    from: './assets/images',
+    to: 'images/[path][name].[ext]',
+  })
 ;
 
 module.exports = Encore.getWebpackConfig();
