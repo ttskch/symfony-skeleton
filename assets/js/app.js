f7548455b89906dd6d99bb6e01a74d9e8c135ce7
Takashi Kanemoto <ttskch@gmail.com>
2020-07-29 12:09:59 +0900

    refactor: 💡 tweak

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,24 +13,13 @@ import '../scss/app.scss';
 
 import 'select2';
 import bsCustomFileInput from 'bs-custom-file-input';
+import applySelect2 from './lib/applySelect2';
 
-// somehow this doesn't work...
-// @see https://select2.org/configuration/data-attributes
-// $('select').select2({
-//   theme: 'bootstrap4',
-//   width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
-//   placeholder: $(this).data('placeholder'),
-//   allowClear: Boolean($(this).data('allow-clear')) || false,
-// });
-$('select').each(function () {
-  $(this).select2({
-    theme: 'bootstrap4',
-    width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
-    placeholder: $(this).data('placeholder'),
-    allowClear: Boolean($(this).data('allow-clear')) || false,
-  });
-});
+// to use jquery from html event listener like onclick
+// @see https://symfony.com/doc/current/frontend/encore/legacy-applications.html
+global.$ = global.jQuery = $;
 
+applySelect2($('select'));
 $('select[autofocus]').select2('focus');
 
 $('[data-toggle="tooltip"]').tooltip();
